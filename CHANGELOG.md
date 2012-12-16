f71c256755a14b7f076cfd1c491b05250ed1a07e
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2012-12-16 18:58:57 +0100

    housekeeping

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG for dotdeb
+
+This file is used to list changes made in each version of dotdeb.
+
+## 0.0.1:
+
+* Initial release of dotdeb
--- default.rb
+++
@@ -1,2 +0,0 @@
-#(Optional) if you want to install PHP 5.4 on Debian 6.0 “Squeeze”, set the following variable to true:
-default['php-fpm']['php54'] = false
